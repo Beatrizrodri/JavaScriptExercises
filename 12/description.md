b71a5c719e91ad097c17ad76a9d9777ee9509c63
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-07-31 21:15:59 -0300

    add exercise 12

---
+++ 12/description.md
@@ -0,0 +1,5 @@
+# Exercício 12
+
+## Descrição
+
+Escreva um programa JavaScript para obter a URL do site (página de carregamento).
---
+++ 12/script.js
@@ -0,0 +1,5 @@
+let url = window.location.href
+
+const resultElement = document.getElementById('display')
+
+resultElement.innerHTML = url
