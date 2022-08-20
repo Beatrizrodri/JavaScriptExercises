dee41da7933922084a6c82e588914903c18411ba
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-08-20 16:24:29 -0300

    add exercise 36

---
+++ 36/description.md
@@ -0,0 +1,5 @@
+# Exercício 36
+
+## Descrição
+
+Escreva um programa JavaScript para verificar se o último dígito dos três inteiros positivos fornecidos é o mesmo.
---
+++ 36/script.js
@@ -0,0 +1,23 @@
+function verifyNumber() {
+  const firstNumberValue = document.getElementById('first-number').value
+
+  const secondNumberValue = document.getElementById('second-number').value
+
+  const thirdNumberValue = document.getElementById('third-number').value
+
+  const resultElement = document.getElementById('display')
+
+  if (firstNumberValue > 0 && secondNumberValue > 0 && thirdNumberValue > 0) {
+    if (
+      String(firstNumberValue.slice(-1)) ==
+        String(secondNumberValue.slice(-1)) &&
+      String(secondNumberValue.slice(-1)) == String(thirdNumberValue.slice(-1))
+    ) {
+      resultElement.textContent = `O último digito é igual`
+    } else {
+      resultElement.textContent = `O último digito é diferente`
+    }
+  } else {
+    resultElement.textContent = `Algum número não é positivo`
+  }
+}
